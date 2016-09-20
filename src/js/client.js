8907bb4dd422a884ddc1d3c44fad183b50a4025d
Cedric ZH <cedriczhchan@gmail.com>
2016-09-20 16:11:12 +0800

    Setup single reducer and store

---
+++ src/js/client.js
@@ -0,0 +1,20 @@
+import { createStore } from 'redux'
+
+const reducer = function(state, action){
+  if (action.type === 'INC'){
+    return state + action.payload;
+  }else if(action.type === 'DEC'){
+    return state - action.payload;
+  }
+  return state;
+}
+
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log('store changed', store.getState())
+})
+
+store.dispatch({ type: 'INC', payload: 1})
+store.dispatch({ type: 'INC', payload: 1})
+store.dispatch({ type: 'DEC', payload: 5})
